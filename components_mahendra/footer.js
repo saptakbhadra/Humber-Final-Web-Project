8240f44e0b5e27bac1cee7288ea5258bfa7ceff6
saptakbhadra <saptak.bhadra@gmail.com>
2022-11-27 21:33:29 -0500

    pusing first version of group project

---
+++ components_mahendra/footer.js
@@ -0,0 +1,77 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <style>
+          footer {
+            text-align: center;
+            background-color: black;
+            color: white;
+            /* position: absolute; */
+            width: 100%;
+            bottom: 0;
+          }
+          #footlist{
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+          }
+          .footdiv{
+            padding: 50px 100px;
+          }
+          .foothead{
+            padding-bottom: 35px;
+            font-size: 18px;
+            font-weight: 400;
+          }
+          footer a{
+            text-decoration: none;
+            color: white;
+          }
+          footer a:hover{
+            color: orange;
+          }
+          /*style for copyright below footer*/
+          #copyr{
+            clear: left;
+            padding-top: 20px;
+            padding-bottom: 30px;
+            border-top: 1px solid white;
+            font-size: 12px;
+          }
+          #copyr a{
+            text-decoration: underline;
+          }
+        </style>
+        <footer>
+            <div id="footlist">
+                <div class="footdiv">
+                    <p class="foothead">Learn More</p>
+                    <a href="History.html">History</a><br><br>
+                    <a href="Contact.html">Contact</a><br><br>
+                </div>
+                <div class="footdiv">
+                    <p class="foothead">About Us</p>
+                    <a href="">About</a><br><br>
+                    <a href="index_utsa.html">Marchandise</a><br><br>
+                </div>
+                <div class="footdiv"> 
+                    <p class="foothead">Follow Us</p>
+                    <a href=""><i class="fa-brands fa-instagram"></i></a><br><br>
+                    <a href=""><i class="fa-brands fa-facebook"></i></a><br><br>
+                </div>
+            </div>
+            <!--copy right at the end of footer-->
+            <div id="copyr">
+            Copyright &copy; 2022 Welcome to Canada<br />
+            <a href="mailto:welcometocanada@w2c.ca">welcometocanada@w2c.ca</a>
+            </div>
+        </footer>
+      `;
+    }
+  }
+  
+  customElements.define('footer-component', Footer);
---
+++ components_mahendra/header.js
@@ -0,0 +1,104 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <style>
+            *{
+                padding: 0;
+                margin: 0;
+                font-family: 'Poppins';
+            }
+            body {
+                background-color: #c8d8e4;
+                /* background-image: url("/images_mahendra/bgimage.jpg"); */
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            header {
+                text-align: center;
+                background-color: black;
+                display: flex;
+                align-items: center;
+                position:fixed;
+                width:100%;
+                top: 0;
+                border-bottom: 1px solid rgb(231, 225, 225);
+            }
+            /*Making logo float left on the header */
+            #logo{
+                margin-left: 20px;
+                width: auto;
+                height: 35px;
+            } 
+            nav{
+                text-align: center;
+                margin: 0 auto;
+            }
+
+            #profile{
+                margin-right: 20px
+            }
+            /* Displaying nav elements at the center*/
+            ul {
+                list-style-type: none;
+                text-align: center;
+                display: inline-block;
+                padding: 22px 110px;
+                font-size: 18px;
+            }
+            li {
+                float: left;
+            }
+            li a {
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 15px 30px;
+                text-decoration: none;
+            }
+            li:last-child{
+                float: right;
+            }
+            nav a:link {
+                color: orange;
+            }
+            nav a:visited {
+                color: white;
+            }
+            /* Adding some selectors for anchor tag to style*/
+            nav a:hover,
+            nav a:focus {
+                color: rgb(0, 0, 255)
+            }
+
+            .navicons{
+                color: white;
+                padding: 10px
+            }
+        </style>
+        <header>
+            <a href="/index.html"><img src="images_mahendra/logo.png" alt="logo" id="logo" /></a>
+            <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Places</a></li>
+                <li><a href="popular-cuisines.html">Food</a></li>
+                <li><a href="Groceries.html">Groceries</a></li>
+                <li><a href="#">Events</a></li>
+                <li></li>
+            </ul>
+            </nav>
+            <!--adding profile and logoff icons at the right side of header-->
+            <div id="profile">
+            <a href="login.html" class="navicons"><i class="fa-regular fa-user"></i></a>
+            <a href="login.html" class="navicons"><i class="fa-solid fa-right-from-bracket"></i></i></a>
+            </div>
+        </header>
+      `;
+    }
+  }
+  
+  customElements.define('header-component', Header);
